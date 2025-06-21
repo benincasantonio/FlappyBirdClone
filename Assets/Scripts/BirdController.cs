810a310a6bf358e8a4a30edf26e2462a08aaf67a
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-06-21 11:41:11 +0200

    feat: add jump input action and jump mechanic

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BirdController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private float jumopForce = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody2D component is missing from the GameObject.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnJump(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            print("Jump action performed");
+            Jump();
+        }
     }
+
+    private void Jump()
+    {
+        print("Jumping with force: " + jumopForce);
+        if (rb != null)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumopForce);
+        }
+        else
+        {
+            Debug.LogError("Rigidbody2D is not assigned. Cannot perform jump.");
+        }
+    }
+    
 }
